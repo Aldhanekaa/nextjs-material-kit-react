f9b2b0f98478ab086a975d07490451dc2b879e8e
aldhanekaa <aldhanekadev@gmail.com>
2021-07-25 21:29:34 +0700

    s

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 // theme
 import ThemeConfig from "../theme";
