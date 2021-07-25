890645d3d8a02d2002828063f5d32d270f85d627
aldhanekaa <aldhanekadev@gmail.com>
2021-07-25 21:06:10 +0700

    fix error

--- src/layouts/dashboard/index.tsx
+++ src/layouts/dashboard/index.tsx
@@ -1,28 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 // material
-import { styled } from '@material-ui/core/styles';
+import { styled } from "@material-ui/core/styles";
 //
-import DashboardNavbar from './DashboardNavbar';
-import DashboardSidebar from './DashboardSidebar';
+import DashboardNavbar from "./DashboardNavbar";
+import DashboardSidebar from "./DashboardSidebar";
 
 // ----------------------------------------------------------------------
 
 const APP_BAR_MOBILE = 64;
 const APP_BAR_DESKTOP = 92;
 
-const RootStyle = styled('div')({
-  display: 'flex',
-  minHeight: '100%',
-  overflow: 'hidden',
+const RootStyle = styled("div")({
+  display: "flex",
+  minHeight: "100%",
+  overflow: "hidden",
 });
 
-const MainStyle = styled('div')(({ theme }) => ({
+const MainStyle = styled("div")(({ theme }) => ({
   flexGrow: 1,
-  overflow: 'auto',
-  minHeight: '100%',
+  overflow: "auto",
+  minHeight: "100%",
   paddingTop: APP_BAR_MOBILE + 24,
   paddingBottom: theme.spacing(10),
-  [theme.breakpoints.up('lg')]: {
+  [theme.breakpoints.up("lg")]: {
     paddingTop: APP_BAR_DESKTOP + 24,
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
@@ -31,6 +31,7 @@ const MainStyle = styled('div')(({ theme }) => ({
 
 // ----------------------------------------------------------------------
 
+// @ts-ignore
 export default function DashboardLayout({ children }) {
   const [open, setOpen] = useState(false);
 
